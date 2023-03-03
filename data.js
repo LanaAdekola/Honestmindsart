2fe85d26803e55bdc1a6474f0f02279183cce488
LanaAdekola <adekolaadelana@gmail.com>
2023-03-03 20:43:13 +0100

    f

--- data.js
+++ data.js
@@ -1,14 +1,16 @@
-[
+export let newArrival = [
   {
     id: "jfhgbvnscs",
     name: "Casual Shirt",
-    price: 45,
+    quantity: 0,
+    price: 20000,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
     img: "images/img-1.jpg",
   },
   {
     id: "ioytrhndcv",
     name: "Office Shirt",
+    quantity: 0,
     price: 100,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
     img: "images/img-2.jpg",
@@ -16,6 +18,7 @@
   {
     id: "wuefbncxbsn",
     name: "T Shirt",
+    quantity: 0,
     price: 25,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
     img: "images/img-3.jpg",
@@ -23,6 +26,7 @@
   {
     id: "thyfhcbcv",
     name: "Mens Suit",
+    quantity: 0,
     price: 300,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
     img: "images/img-4.jpg",
@@ -30,6 +34,7 @@
   {
     id: "thiecbawdjksadjk",
     name: "Mens Tie",
+    quantity: 0,
     price: 25,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
     img: "images/img-5.png",
@@ -37,6 +42,7 @@
   {
     id: "iuertrywebncdjksadjk",
     name: "Casual shoes",
+    quantity: 0,
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
     img: "images/img-6.png",
@@ -44,13 +50,16 @@
   {
     id: "thierytbvcbvzdhadjk",
     name: "black suit",
+    quantity: 0,
     price: 450,
+    quantity: 27,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
     img: "images/img-7.png",
   },
   {
     id: "trfoiwfcnbcawdjksadjk",
     name: "polo shirt",
+    quantity: 0,
     price: 45,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
     img: "images/img-8.png",
@@ -58,6 +67,7 @@
   {
     id: "cbvxbcvsceldk",
     name: "denim shirt",
+    quantity: 0,
     price: 85,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
     img: "images/img-9.png",
@@ -65,6 +75,7 @@
   {
     id: "oiopijmjkhuihb",
     name: "denim pants",
+    quantity: 0,
     price: 120,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
     img: "images/img-10.png",
@@ -72,6 +83,7 @@
   {
     id: "oiopijewyiohbjhib",
     name: "basic cap",
+    quantity: 0,
     price: 35,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
     img: "images/img-11.png",
@@ -79,6 +91,7 @@
   {
     id: "rtytytuyuytyytbvncv",
     name: "leather boots",
+    quantity: 0,
     price: 350,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
     img: "images/img-12.png",
